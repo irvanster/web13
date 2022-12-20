f6851a512351776bb7dc294c4c9adf70931601ba
Tova Maulana Irvan <tovamaulana098@gmail.com>
2022-12-20 09:40:18 +0700

    feat: week 3

--- week3/js2/cb.js
+++ week3/js2/cb.js
@@ -26,6 +26,7 @@ const posts = [
 //     });
 // }
 const getData = (data, param2) => {
+    //fungsi inti perulangan
     // return param2(data)
     data.forEach(element => {
         return param2(element)
@@ -36,9 +37,9 @@ const getData = (data, param2) => {
 //     return params
 // }
 // getData(['adriel','letto'], tampilkanText)
-getData(['adriel','letto'], (item)=> {
-    console.log(item)
-})
+// getData(['adriel','letto'], (item)=> {
+//     console.log(item)
+// })
 
 // const posts = {
 //     data: [    {title: 'Elon Musk beli twitter'},
@@ -62,3 +63,34 @@ getData(['adriel','letto'], (item)=> {
 // getData(posts, (data)=> {
 //     console.log(data)
 // })
+
+const searchBook = (param1, param2, cb)=> {
+    //fungsi inti
+    const name = [
+        'cinta', 'sayang', 'galau',
+        'manga', 'anime', 'donghua','cinta kasih',
+    ]
+    //untuk membuat agar nama, search, olahan
+    //internet aku down
+    const search = '' //logic untuk searching
+    const logic = name.slice(0, param2) //logic untuk pembatasan
+    const sorting = logic.sort()
+
+    return cb(sorting) //argument untuk manggil showResult
+
+}
+
+const showResult = (data)=> {
+        console.log(data)
+} 
+searchBook('CINTAa', 3, showResult)
+//meneruskan
+// fungsi yang dijadikan sebuah parameter
+//
+
+
+// const showResult = (data)=> {
+//     console.log(data)
+// }
+
+// showResult('oawkwkoakowakowakowako')
